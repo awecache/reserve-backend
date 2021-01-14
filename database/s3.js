ca156538946aae867109a8454e85e06c3cc44c8e
Steven <stevenzh2020@gmail.com>
2021-01-15 06:07:55 +0800

    removed config.json

--- database/s3.js
+++ database/s3.js
@@ -1,13 +1,13 @@
 const aws = require('aws-sdk');
-const config = require('../config.json');
+// const config = require('../config.json');
 const { AWS_S3_HOSTNAME, AWS_S3_ACCESS_KEY, AWS_S3_SECRET } = process.env;
 
 const spacesEndpoint = new aws.Endpoint(AWS_S3_HOSTNAME);
 
 const s3 = new aws.S3({
-  endpoint: spacesEndpoint,
-  accessKeyId: config.accessKeyId || AWS_S3_ACCESS_KEY,
-  secretAcessKey: config.secretAccessKey || AWS_S3_SECRET
+  endpoint: spacesEndpoint
+  // accessKeyId: config.accessKeyId || AWS_S3_ACCESS_KEY,
+  // secretAcessKey: config.secretAccessKey || AWS_S3_SECRET
 });
 
 module.exports = s3;
